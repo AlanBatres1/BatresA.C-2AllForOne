e19880c4bcaa3cb73b9b620d6d83ea618b35f6a2
AlanBatres1 <alanbatres328@gmail.com>
2025-02-07 14:20:49 -0800

    fixed

--- obj/Release/net8.0/BatresA.C#2AllForOne.AssemblyInfo.cs
+++ obj/Release/net8.0/BatresA.C#2AllForOne.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BatresA.C#2AllForOne")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2dc98f7deb0b9a5016ec9a34b711d6f88cbf595e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ff82b6f8a6b92d4c4135d7a2440b8d8ea9c47c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BatresA.C#2AllForOne")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BatresA.C#2AllForOne")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
